7280d59c5a180366b6e44c3762614a22768e2701
Borja Fernández Gauna <bortxkudai@hotmail.com>
2019-04-14 16:23:09 +0200

    Add project files.

---
+++ VSLinuxMakefiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSLinuxMakefiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
