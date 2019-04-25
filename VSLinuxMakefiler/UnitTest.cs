9bde7d713cbc67f08f6373156096b0fa0b5f41e1
Borja Fernández Gauna <borjafdezgauna>
2019-04-25 18:40:55 +0200

    #2 Preprocessed source files ready to be compiled under linux are correctly output now

--- VSLinuxMakefiler/UnitTest.cs
+++ VSLinuxMakefiler/UnitTest.cs
@@ -20,11 +20,60 @@ namespace VSLinuxMakefiler
 
         string LinuxCppUnitTestHeaderDir { get; }
 
+        List<UnitTestNamespace> m_unitTestNamespaces = new List<UnitTestNamespace>();
+        List<string> m_unitTestSourceFiles = new List<string>();
+
         public UnitTest(string name, string projectPath, string solutionPath, string linuxCppUnitTestHeaderDir): base(name, projectPath, solutionPath)
         {
             LinuxCppUnitTestHeaderDir = linuxCppUnitTestHeaderDir;
-            //Preprocess source files and create the source file to actually be compiled
+
+            //Parse source files to extract test unit Namespace/Class/Methods
+            foreach (string sourceFile in SourceFiles)
+            {
+                string solutionRelPathToSourceFile = SolutionRelativePathToSourceFile(sourceFile);
+                string absolutePathToSourceFile = solutionPath + "\\" + solutionRelPathToSourceFile;
+                List<UnitTestNamespace> namespaces= VSUnitTestProjectParser.Parse(absolutePathToSourceFile);
+
+                if (namespaces != null && namespaces.Count>0)
+                {
+                    m_unitTestNamespaces.AddRange(namespaces);
+                    m_unitTestSourceFiles.Add(sourceFile);
+                }
+            }
+
+            //Create the ready-to-compile preprocessed source file
+            string outputPreprocessedSourceFile = solutionPath + "\\" + PreprocessedSourceFile;
+            using (StreamWriter writer = File.CreateText(outputPreprocessedSourceFile))
+            {
+                writer.WriteLine(OutputFileHeader);
+                foreach (string unitTestSourceFile in m_unitTestSourceFiles)
+                    writer.WriteLine(OutputFileIncludeUnitTestSource, unitTestSourceFile);
+                foreach (UnitTestNamespace unitTestNamespace in m_unitTestNamespaces)
+                    writer.WriteLine(OutputFileUsingNamespace, unitTestNamespace.Name);
+                writer.WriteLine(OutputFileBeginMain);
+                foreach (UnitTestNamespace unitTestNamespace in m_unitTestNamespaces)
+                {
+                    foreach(UnitTestClass unitTestClass in unitTestNamespace.Classes)
+                    {
+                        foreach (string unitTestMethod in unitTestClass.Methods)
+                        {
+                            writer.WriteLine(string.Format(OutputFileTestMethod, unitTestClass.Name, unitTestMethod));
+                        }
+                    }
+                }
+                writer.WriteLine(OutputFileEndMain);
+            }
         }
+
+        //Constants used to generate the preprocessed source file
+        public const string OutputFileHeader = "#include <iostream>";
+        public const string OutputFileIncludeUnitTestSource = "#include \"{0}\"";
+        public const string OutputFileUsingNamespace = "using namespace {0};";
+        public const string OutputFileIncludeUnit = "#include \"{0}\"";
+        public const string OutputFileBeginMain = "int main()\n{\n  bool success= true;\n";
+        public const string OutputFileEndMain = "  return success;\n}";
+        public const string OutputFileTestMethod = "  try\n  {{\n    {0}::{1}();\n    std::cout << \"Passed {0}::{1}()\\n\";\n  }}\n  catch(std::runtime_error error)\n  {{\n    success= false;\n"
+            + "    std::cout << \"Failed {0}::{1}()\\n\";\n  }}";
 
         public override string CompilerFlags(string sourceFile)
         {
---
+++ VSLinuxMakefiler/VSUnitTestProjectParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace VSLinuxMakefiler
+{
+    public class UnitTestClass
+    {
+        public string Name { get; } = null;
+        public List<string> Methods { get; } = new List<string>();
+
+        const string testMethodRegEx = @"TEST_METHOD\s*\((\w+)\)\s*\r*\n*\{((?:[^{}]|(?<open>\{)|(?<-open>\}))+(?(open)(?!)))\}";
+
+        public UnitTestClass(string name,string classContent)
+        {
+            Name = name;
+            foreach (Match match in Regex.Matches(classContent, testMethodRegEx))
+                Methods.Add(match.Groups[1].Value);
+        }
+    }
+    public class UnitTestNamespace
+    {
+        public string Name { get; } = null;
+        public List<UnitTestClass> Classes { get; } = new List<UnitTestClass>();
+
+        const string classRegEx = @"TEST_CLASS\s*\((\w+)\)\s*\r*\n*\{((?:[^{}]|(?<open>\{)|(?<-open>\}))+(?(open)(?!)))\}";
+
+        public UnitTestNamespace(string name, string namespaceContent)
+        {
+            Name = name;
+            foreach (Match match in Regex.Matches(namespaceContent, classRegEx))
+                Classes.Add(new UnitTestClass(match.Groups[1].Value, match.Groups[2].Value));
+        }
+    }
+
+    public class VSUnitTestProjectParser
+    {
+        const string namespaceRegEx = @"namespace\s+([\w\.]+)\s*\r*\n*\{((?:[^{}]|(?<open>\{)|(?<-open>\}))+(?(open)(?!)))\}";
+
+        static public List<UnitTestNamespace> Parse (string sourceFile)
+        {
+            string sourceFileCode = File.ReadAllText(sourceFile);
+
+            List<UnitTestNamespace> namespaces = new List<UnitTestNamespace>();
+
+            foreach (Match match in Regex.Matches(sourceFileCode, namespaceRegEx))
+                namespaces.Add(new UnitTestNamespace(match.Groups[1].Value, match.Groups[2].Value));
+
+            return namespaces;
+        }
+    }
+}
